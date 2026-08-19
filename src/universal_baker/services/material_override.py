76b9221952a389578c55a1ca3818aeeebd37189f
Tilapiatsu <tilapiatsu@hotmail.fr>
2026-08-19 11:57:12 +0200

    + add debug log to material override

--- src/universal_baker/services/material_override.py
+++ src/universal_baker/services/material_override.py
@@ -4,6 +4,10 @@ from dataclasses import dataclass
 
 import bpy
 
+from ..constant import LOG
+
+LOG_SCOPE = "Material Overide"
+
 
 @dataclass
 class MaterialSnapshot:
@@ -17,46 +21,51 @@ class MaterialOverrideService:
         material: bpy.types.Material,
     ) -> list[MaterialSnapshot]:
 
-        # ISSUE: When restoring, the previous materials didn't get reapplied to the objects properly
-        snapshots = []
+        with LOG.scope(LOG_SCOPE):
+            # ISSUE: When restoring, the previous materials didn't get reapplied to the objects properly
+            snapshots = []
 
-        for obj in bpy.context.scene.objects:
-            if obj.type != "MESH":
-                continue
+            for obj in bpy.context.scene.objects:
+                if obj.type != "MESH":
+                    continue
 
-            snapshots.append(
-                MaterialSnapshot(
-                    object=obj,
-                    materials=[slot.material for slot in obj.material_slots],
+                LOG.debug(f"Storing materials for {obj.name}")
+                snapshots.append(
+                    MaterialSnapshot(
+                        object=obj,
+                        materials=[slot.material for slot in obj.material_slots],
+                    )
                 )
-            )
 
-            # NOTE: add a Material slot if missing
-            if len(obj.material_slots) == 0:
-                obj.data.materials.append(None)
+                # NOTE: add a Material slot if missing
+                if len(obj.material_slots) == 0:
+                    obj.data.materials.append(None)
 
-            for slot in obj.material_slots:
-                slot.material = material
+                for index, slot in enumerate(obj.material_slots):
+                    LOG.debug(f"Apply material {material.name} to slot {index}")
+                    slot.material = material
 
-        return snapshots
+            return snapshots
 
     @staticmethod
     def restore(
         snapshots: list[MaterialSnapshot],
     ):
 
-        for snapshot in snapshots:
-            obj = snapshot.object
+        with LOG.scope(LOG_SCOPE):
+            for snapshot in snapshots:
+                obj = snapshot.object
 
-            if obj is None:
-                continue
-
-            # NOTE: Remove all materials the object didn't had materials at the first place
-            if not len(snapshot.materials):
-                obj.data.materials.clear()
-
-            for index, material in enumerate(snapshot.materials):
-                if index >= len(obj.material_slots):
+                if obj is None:
                     continue
 
-                obj.material_slots[index].material = material
+                # NOTE: Remove all materials the object didn't had materials at the first place
+                if not len(snapshot.materials):
+                    obj.data.materials.clear()
+
+                for index, material in enumerate(snapshot.materials):
+                    if index >= len(obj.material_slots):
+                        continue
+
+                    LOG.debug(f"Restoring material slot {index} : {material.name if material is not None else 'EMPTY'}")
+                    obj.material_slots[index].material = material
